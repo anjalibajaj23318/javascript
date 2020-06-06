965425ab20b6657387a8d0ce4bfa1fba8d4e38c8
anjalibajaj23318 <34480619+anjalibajaj23318@users.noreply.github.com>
2020-06-06 19:05:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,99 @@
+/*var myarray=["eating","watching","listening","learning"];
+
+var progress=["done","in progress","new"];
+
+var question1=prompt("which task"+myarray.length);
+
+var question2=prompt("status?");
+
+
+console.log("today you will "+myarray[question1]+"status ="+progress[question2]);
+
+
+
+var animals=[[3,4,5,1,55,8,0,2],[5,6,7,8,9,0]];
+var obj={color:"read",
+        make:"ford",
+        wheels:6};//literal
+console.log(obj);
+
+var obj1=new Object();
+console.log(obj1);
+
+for(var car in obj){
+console.log(obj[car]);
+}
+animals.sort();
+var q=prompt("what do you want to remove");
+var finder=animals.indexOf(q);
+var rem=animals.splice(finder,1);
+
+console.log(animals);
+var randomindex=Math.floor(Math.random()*animals.length);
+var randomanimal=(animals[randomindex]);
+
+var message=prompt("which index value is"+randomanimal);
+console.log(message==randomindex?true:false);
+
+
+animals.push("goat");
+
+animals.pop();
+
+animals.shift();
+
+animals.unshift("dog");
+
+console.log(animals);
+
+animals.sort();
+animals.reverse();
+
+
+//animals.sort().reverse();
+
+//animals[20]="tiger";
+//animals.fill("wolf",6,20);
+
+
+var bob={age:30,hair:"brown",eyes:"blue",works:"developer"};
+var friends={friend1:bob,friend2:{age:30,hair:"brown",eyes:"blue"}};
+
+var friends1=[bob,{age:30,hair:"black"}];
+
+
+
+
+
+
+
+*/
+
+console.dir(document);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
